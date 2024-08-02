4c91dc7a03b2a3fa6bf78df3786fa449780e47ee
jimi <jimi96@softworks.co.kr>
2024-08-02 17:39:40 +0900

    [24.08.02] 중간저장

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,160 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo"/>
+  </div>
+
+  <Container :data="data" :step="step"/>
+  <button @click="more" v-if=" step===0 ">더보기</button>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input @change="upload" type="file" id="file" class="inputfile"/>
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from "@/components/Container.vue";
+import data from "@/assets/data"
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      data,
+      moreNum: 0,
+      step: 0
+    }
+  },
   components: {
-    HelloWorld
+    Container
+  },
+  methods: {
+    // 더보기 버튼 클릭
+    more() {
+      if (this.moreNum > 1) {
+        console.log('더보기는 2개까지 가능');
+        return;
+      }
+
+      console.log('ajax 요청')
+
+      axios.get(`https://codingapple1.github.io/vue/more${this.moreNum}.json`)
+          .then((res) => {
+            this.data.push(res.data);
+            this.moreNum++;
+          })
+          .catch((err => {
+            // 요청 실패 시 실행할 코드
+            console.log('요청에 실패하였습니다 : ', err)
+          }))
+    },
+    upload(e) {
+      let file = e.target.files
+      this.step = 1; // 업로드 후 다음 페이지로 이동
+      let url = URL.createObjectURL(file[0]); // 업로드한 이미지의 URL 생성
+      console.log('url > ', url)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+body {
+  margin: 0;
+}
+
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+
+.inputfile {
+  display: none;
+}
+
+.input-plus {
+  cursor: pointer;
+}
+
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
   margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
 }
 </style>
