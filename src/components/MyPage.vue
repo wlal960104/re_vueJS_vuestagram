a1c13e79a093a22fe4e3e85511f8aa85e01d3696
jimi <jimi96@softworks.co.kr>
2024-08-06 17:10:45 +0900

    [24.08.02][part3] 17강 숙제 중간 저장

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,9 +1,10 @@
 <template>
   <!-- Composition API 실습 -->
-  {{ follower }}
   <div style="padding : 10px">
     <h4>팔로워</h4>
-    <input placeholder="검색"/>
+    {{ name }}
+    <input v-model="name" placeholder="검색"/>
+
     <div v-for="(a,i) in follower" :key="i" class="post-header">
       <div :style="`background-image: url(${a.image})`" class="profile"></div>
       <span class="profile-name">{{ a.name }}</span>
@@ -19,8 +20,32 @@ import {useStore} from "vuex";
 export default {
   name: 'MyPage',
   setup(props) {
+    // // array 테스트
+    // let arr = ['_Limvely', 'salmon_X', '360noscope'];
+    // console.log('arr > ', arr);
+    //
+    //
+    // let result = arr.filter((arr)=>{
+    //   console.log('arr > ', arr);
+    //   return arr.includes('noscope');
+    // })
+    // console.log('result > ', result)
+
+    let followerOrigin = []; // 원본 follower 저장
     let follower = ref([]);
+
     let test = reactive({name: 'kim'}) // reactive 사용
+    let name = ref('검색');
+
+    // 함수 사용하기
+    function doThis () {
+      console.log('doThis 함수입니다');
+    }
+
+    // 유저 검색 기능
+    // function search () {
+    //
+    // }
 
     // props 사용하기
     let {one, two} = toRefs(props);
@@ -28,8 +53,39 @@ export default {
     console.log('two >', two.value);
 
     // watch 사용하기
-    watch(one, () => {
-      // one 데이터가 변경될 때마다 실행되는 코드
+    watch(name, (a) => {
+      if (a === '') {
+        follower.value = [...followerOrigin];
+      }
+      console.log('<<<<<<<<<<<< 변경되는 중 >>>>>>>>>>>>> ', a);
+      // 데이터가 변경될 때마다 실행되는 코드
+      let followers = [...follower.value];
+      console.log('followers >' , followers)
+
+      console.log('follower.value > ', follower.value);
+
+       followers = followers.filter((arr) => {
+        console.log('검색어 >>> ', a)
+        console.log('arr.name > ', arr.name)
+        return arr.name.includes(a);
+      })
+      if (followers.length >= 1 ){
+        follower.value = followers;
+      } else {
+        follower.value = [];
+      }
+
+
+      console.log('followers', followers);
+      console.log('followers.length',followers.length);
+
+
+      console.log('--------------------------------------')
+      console.log('followers', followers);
+      console.log('follower > ', follower.value);
+      console.log('원본 followerOrigin > ', followerOrigin)
+
+
     })
 
    // computed 사용하기
@@ -46,11 +102,31 @@ export default {
 
     onMounted(() => {
       axios.get('/follower.json').then((res) => {
-        follower.value = res.data; // 데이터 담고 싶다면 변수명.value
+        console.log('res.data > ', res.data);
+        followerOrigin = [...(res.data)];
+        follower.value = [...(res.data)]
+
+
+
+        // // TODO: 왜 원본 데이터 저장 안되지
+        // followerOrigin = [...res.data]; // 원본 데이터 저장
+        // follower = [...res.data]; // 데이터 담고 싶다면 변수명.value
+        // followerOrigin[0].name = 'nana';
+        // console.log('followerOrigin >>>>>>>>> ', followerOrigin);
+        // console.log('follower.value >>>>>>>>>>>', follower);
       })
+
+      // let array = [1, 2, 3, 4, 5, 6];
+      // let a = [...array];
+      // let b = [...array];
+      // a[0] = 100
+      // console.log(' a >>> ', a);
+      // console.log(' b >>>', b)
+
+
     })
 
-    return {follower,test}
+    return {follower, test, doThis, name}
   },
   props : {
     one : Number,
